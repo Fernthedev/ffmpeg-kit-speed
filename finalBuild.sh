6927dcea647e64d590a2527e045fa5d4bebc2221
Fernthedev <15272073+Fernthedev@users.noreply.github.com>
2021-04-10 21:13:38 -0400

    Fix autoreconf for speex

--- finalBuild.sh
+++ finalBuild.sh
@@ -1,2 +1,2 @@
 cd mobile-ffmpeg 
-./android.sh --enable-gpl --full --disable-arm-v7a --disable-arm-v7a-neon --disable-x86 --disable-x86-64 -s --reconf-shine
+./android.sh --enable-gpl --full --disable-arm-v7a --disable-arm-v7a-neon --disable-x86 --disable-x86-64 -s --reconf-shine --reconf-speex
