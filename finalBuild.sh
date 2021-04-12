662e998d76f40621d4d742199b874c7ddb56f629
Fernthedev <15272073+Fernthedev@users.noreply.github.com>
2021-04-11 23:45:00 -0400

    Maybe static libraries? likely to fail

--- finalBuild.sh
+++ finalBuild.sh
@@ -1,2 +1,4 @@
 cd mobile-ffmpeg 
+git apply ../static_link_build.patch
+
 ./android.sh --enable-gpl --full --disable-arm-v7a --disable-arm-v7a-neon --disable-x86 --disable-x86-64 -s --reconf-shine --reconf-speex --reconf-libilbc --reconf-giflib --reconf-libuuid --reconf-leptonica --reconf-libass --reconf-libsamplerate
