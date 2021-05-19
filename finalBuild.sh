7a6ce0e0ade6980405510bf65316e625db7e30b8
Fernthedev <15272073+Fernthedev@users.noreply.github.com>
2021-05-19 15:50:36 -0400

    Avoid using OpenH264 since we don't want to redistribute that

--- finalBuild.sh
+++ finalBuild.sh
@@ -2,4 +2,4 @@ cd ffmpeg-kit
 # git apply ../static_link_build.patch
 
 # ./android.sh --enable-gpl --full --disable-arm-v7a --disable-arm-v7a-neon --disable-x86 --disable-x86-64 -s --reconf-shine --reconf-speex --reconf-libilbc --reconf-giflib --reconf-libuuid --reconf-leptonica --reconf-libass --reconf-libsamplerate
-./android.sh --speed --full --enable-gpl --enable-x264 --enable-x265 --disable-arm-v7a --disable-arm-v7a-neon --disable-x86 --disable-x86-64
+./android.sh --speed --full --enable-gpl --skip-openh264 --enable-x264 --enable-x265 --disable-arm-v7a --disable-arm-v7a-neon --disable-x86 --disable-x86-64 
