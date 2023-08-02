12bd66ded1cb8a879dae1257c29e4d43a4d2fd58
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2023-08-02 16:39:09 -0400

    Final build lite

--- finalBuild_lite.sh
+++ finalBuild_lite.sh
@@ -2,4 +2,5 @@ cd ffmpeg-kit
 git apply ../finalBuild.patch
 git apply ../lite_build.patch
 
-./android.sh --speed --full --enable-gpl --skip-openh264 --enable-x264 --enable-x265 --disable-arm-v7a --disable-arm-v7a-neon --disable-x86 --disable-x86-64 --enable-android-media-codec
+# ./android.sh --speed --full --enable-gpl --skip-openh264 --enable-x264 --enable-x265 --disable-arm-v7a --disable-arm-v7a-neon --disable-x86 --disable-x86-64 --enable-android-media-codec
+./android.sh --disable-x86 --disable-x86-64 --disable-arm-v7a-neon --disable-arm-v7a --speed --enable-x265 --enable-gpl
