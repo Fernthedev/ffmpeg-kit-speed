332c3f1ccbb7300fbdc6824a906a32dcc94ab0b1
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2023-08-02 16:42:28 -0400

    Maybe fix scripts

--- finalBuild_lite.sh
+++ finalBuild_lite.sh
@@ -3,4 +3,4 @@ git apply ../finalBuild.patch
 git apply ../lite_build.patch
 
 # ./android.sh --speed --full --enable-gpl --skip-openh264 --enable-x264 --enable-x265 --disable-arm-v7a --disable-arm-v7a-neon --disable-x86 --disable-x86-64 --enable-android-media-codec
-./android.sh --disable-x86 --disable-x86-64 --disable-arm-v7a-neon --disable-arm-v7a --speed --enable-x265 --enable-gpl
+./android.sh --disable-x86 --disable-x86-64 --disable-arm-v7a-neon --disable-arm-v7a --speed --enable-x265 --enable-gpl  --enable-android-media-codec --enable-android-zlib
